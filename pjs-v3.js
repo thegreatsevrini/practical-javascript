8658560a9ac798353c96fc3514b128bdc3abbc9d
sev the dev <sevwelker@gmail.com>
2019-10-03 23:06:43 -0700

    added toggleCompleted method to todoList

--- pjs-v3.js
+++ pjs-v3.js
@@ -20,12 +20,16 @@ let todoList = {
         this.displayTodos();
     },
     changeTodo: function(position, todoText) {
-       // this.todos[position] = newValue;
         this.todos[position].todoText = todoText;
         this.displayTodos();
     },
     deleteTodo: function(position) {
         this.todos.splice(position, 1);
+        this.displayTodos();
+    },
+    toggleCompleted: function(position) {
+        let todo = this.todos[position];
+        todo.completed = !todo.completed;
         this.displayTodos();
     }
 };
