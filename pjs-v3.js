d4f9fe457befa6307579a4b36107f310b6b95bda
sev the dev <sevwelker@gmail.com>
2019-10-09 21:47:36 -0700

    displayTodos logs message if todos is empty and otherwise checks and logs if each loop is completed or not

--- pjs-v3.js
+++ pjs-v3.js
@@ -9,12 +9,26 @@ The purpose of this is to learn JS. Not my own code. Just and exciting exercise.
 
 let todoList = {
     todos: [],
+    //logs message if there are 0 todos otherwise,
+    //determines length of todos and loops through them and
+    //logs todos todoText property to the console for each one. 
     displayTodos: function() {
-        console.log('My Todos:');
-        for(var i = 0; i < this.todos.length; i++) {
-            console.log(this.todos[i].todoText);
-        }
+        
+        if ( this.todos.length === 0 ) {
+            console.log( 'Your todo list is empty!' );
+        } else {
+            console.log('My Todos:');
+            for( var i = 0; i < this.todos.length; i++ ) {
+                //check if .completed is true or false for each for loop
+                if ( this.todos[i].completed === true ) {
+                    console.log( '(x)', this.todos[i].todoText );
+                } else {
+                    console.log( '( )', this.todos[i].todoText );
+                }   
+            } 
+         }   
     },
+    //adds todo object to todos array
     addTodo: function(todoText) {
         this.todos.push({
             todoText: todoText,
@@ -22,10 +36,12 @@ let todoList = {
         });
         this.displayTodos();
     },
+    //changes the todoText property in the given array index to new todoText
     changeTodo: function(position, todoText) {
         this.todos[position].todoText = todoText;
         this.displayTodos();
     },
+    //deletes todo object in given array index
     deleteTodo: function(position) {
         this.todos.splice(position, 1);
         this.displayTodos();
