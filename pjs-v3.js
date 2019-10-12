92a15b4cae6f07d955bc8f5be631d86d66606d55
sev the dev <sevwelker@gmail.com>
2019-10-11 21:01:35 -0700

    added buttons for deleting and toggling todos

--- pjs-v3.js
+++ pjs-v3.js
@@ -80,13 +80,30 @@ let handlers = {
     displayTodos: function () {
         todoList.displayTodos();
     },
-    toggleAll: function(){
-        todoList.toggleAll();
-    },
     addTodo: function(){
         let addTodoTextInput = document.getElementById('addTodoTextInput');
         todoList.addTodo(addTodoTextInput.value);
         addTodoTextInput.value = '';
+    },
+    changeTodo: function() {
+        var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
+        var changeTodoTextInput = document.getElementById('changeTodoTextInput');
+        todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
+        changeTodoPositionInput.value = '';
+        changeTodoTextInput.value = '';
+    }, 
+    deleteTodo: function() {
+        var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
+        todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
+        deleteTodoPositionInput.value = '';
+    },
+    toggleCompleted: function() {
+        var toggleCompletedPosiitonInput = document.getElementById('toggleCompletedPositionInput');
+        todoList.toggleCompleted(toggleCompletedPosiitonInput.valueAsNumber);
+        toggleCompletedPosiitonInput.value = '';
+    },
+    toggleAll: function(){
+        todoList.toggleAll();
     }
 };
 
